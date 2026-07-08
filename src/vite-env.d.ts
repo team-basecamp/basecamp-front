898f42be0fabe08129c68dbbf30f95b3699ee659
catapillar0505 <twrp3301@gmail.com>
2026-07-08 15:08:21 +0900

    feat: 카카오 소셜 로그인 실연동 (authorize 리다이렉트 + 콜백 + 백엔드 계약 정렬)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_KAKAO_MAP_KEY: string;
   readonly VITE_KAKAO_LOGIN_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_NAVER_CLIENT_ID: string;
 }
